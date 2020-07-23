b4ac1d28c1c4134aead837e16284e9e37796cf74
Mgamerz <mgamerz2@gmail.com>
2020-07-22 19:04:05 -0600

    Experiments: Xbox input logger

--- Experiments/Experiments.cpp
+++ Experiments/Experiments.cpp
@@ -35,95 +35,103 @@ FVector inline RotationToVector(FRotator Rotation) {
 void __fastcall HookedPE(UObject* pObject, void* edx, UFunction* pFunction, void* pParms, void* pResult)
 {
 	char* szName = pFunction->GetFullName();
-	if (isPartOf(szName, "Function Engine.DebugCameraHUD.PostRender"))
-	{
-		ProcessEvent(pObject, pFunction, pParms, pResult);
-		ADebugCameraHUD* debugCameraHud = (ADebugCameraHUD*)pObject;
-		if (!debugCameraHud->Canvas)
-		{
-			return;
-		}
-		int xpos = 40;
-		int ypos = 300;
-
-		debugCameraHud->Canvas->SetPos(xpos, ypos); //set canvas position to (10,10)
-		debugCameraHud->Canvas->SetDrawColor(150, 150, 255, 255);
-		debugCameraHud->Canvas->DrawText(L"ME3Tweaks Enhanced Debug Camera Mod", true, 1.5f, 1.5f, NULL);
-
-
-		xpos += 25;
-		ypos += 20;
-		debugCameraHud->Canvas->SetPos(xpos, ypos); //set canvas position to (10,10)
-		debugCameraHud->Canvas->SetDrawColor(255, 255, 255, 255);
-
-		//Perform trace
-
-		FVector Extent = { 0,0,0 };
-		FVector HitLocation;
-		FVector HitNormal;
-		FTraceHitInfo HitInfo;
-		FRotator camRotation = debugCameraHud->PlayerOwner->PlayerCamera->CameraCache.POV.Rotation;
-
-
-		FVector end = RotationToVector(camRotation);
-
-		FVector camLoc = debugCameraHud->PlayerOwner->PlayerCamera->CameraCache.POV.location;
-		end.X = end.X * 5000 * 20 + camLoc.X;
-		end.Y = end.Z * 5000 * 20 + camLoc.Y;
-		end.Z = end.Z * 5000 * 20 + camLoc.Z;
-
-		AActor* foundActor = debugCameraHud->Trace(end, camLoc, 1, Extent, 0, &HitLocation, &HitNormal, &HitInfo);
-		if (foundActor)
-		{
-			string str = string_format("Containing file: %s.pcc", foundActor->GetContainingMapName());
-			std::wstring widestr = std::wstring(str.begin(), str.end());
-			debugCameraHud->Canvas->DrawText(FString(const_cast<wchar_t*>(widestr.c_str())), true, 1.0f, 1.0f, NULL);
-
-			ypos += 12;
-			debugCameraHud->Canvas->SetPos(xpos, ypos); //set canvas position to (10,10)
-			str = string_format("Hit Actor: %s_%i", foundActor->GetFullNameNoClass(), (foundActor->Name.GetIndex() - 1));
-			widestr = std::wstring(str.begin(), str.end());
-			debugCameraHud->Canvas->DrawText(FString(const_cast<wchar_t*>(widestr.c_str())), true, 1.0f, 1.0f, NULL);
-
-
-			if (HitInfo.HitComponent) {
-				ypos += 12;
-				debugCameraHud->Canvas->SetPos(xpos, ypos); //set canvas position to (10,10)
-				str = string_format("Hit component: %s_%d", HitInfo.HitComponent->GetFullName(), HitInfo.HitComponent->Name.GetIndex() - 1);
-				widestr = std::wstring(str.begin(), str.end());
-				debugCameraHud->Canvas->DrawText(FString(const_cast<wchar_t*>(widestr.c_str())), false, 1.0f, 1.0f, NULL);
-			//	HitInfo.HitComponent->
-			}
-
-			//logger.writeToLog(string_format("(%s) %s_%d\n", mapname, fullname, instanceIndex), true);
-		}
-		else
-		{
-			debugCameraHud->Canvas->DrawText(L"No trace hit", true, 1.0f, 1.0f, NULL);
-
-		}
-		return; // do not process twice.
-		/*AActor* actor = (AActor*)pObject;
-		if (actor->WorldInfo)
-		{
-			if (actor->WorldInfo->bUseConsoleInput == 0)
-			{
-				logger.writeToConsoleOnly(string_format(" >> WI IS ZERO! WorldInfo.bUseConsoleInput: %u\n", actor->WorldInfo->bUseConsoleInput), true);
-			}
-			actor->WorldInfo->bUseConsoleInput = 1;
-		}*/
-	}
-	//else if (isPartOf(szName, "Function SFXGame.SFXConsole.InputKey")) {
-	//	USFXConsole_execInputKey_Parms* params = (USFXConsole_execInputKey_Parms*)pParms;
-	//	//Event: 0 - > Down
-	//	//Event: 1 - > Up
-	//	//Event: 2 - > Holding
-	//	logger.writeToConsoleOnly(string_format("Key pressed: %u %s\n", params->Event, params->Key.GetName()), true);
-	//	if (params->Event == 1 && isPartOf(params->Key.GetName(), "Xbox")) {
-	//		logger.writeToConsoleOnly(string_format("%u %s, setting bUseConsoleInput on next tick!\n", params->Event, params->Key.GetName()), true);
-	//		logNextTick = true;
+	//if (isPartOf(szName, "Function Engine.DebugCameraHUD.PostRender"))
+	//{
+	//	ProcessEvent(pObject, pFunction, pParms, pResult);
+	//	ADebugCameraHUD* debugCameraHud = (ADebugCameraHUD*)pObject;
+	//	if (!debugCameraHud->Canvas)
+	//	{
+	//		return;
 	//	}
+	//	int xpos = 40;
+	//	int ypos = 300;
+
+	//	debugCameraHud->Canvas->SetPos(xpos, ypos); //set canvas position to (10,10)
+	//	debugCameraHud->Canvas->SetDrawColor(150, 150, 255, 255);
+	//	debugCameraHud->Canvas->DrawText(L"ME3Tweaks Enhanced Debug Camera Mod", true, 1.5f, 1.5f, NULL);
+
+
+	//	xpos += 25;
+	//	ypos += 20;
+	//	debugCameraHud->Canvas->SetPos(xpos, ypos); //set canvas position to (10,10)
+	//	debugCameraHud->Canvas->SetDrawColor(255, 255, 255, 255);
+
+	//	//Perform trace
+
+	//	FVector Extent = { 0,0,0 };
+	//	FVector HitLocation;
+	//	FVector HitNormal;
+	//	FTraceHitInfo HitInfo;
+	//	FRotator camRotation = debugCameraHud->PlayerOwner->PlayerCamera->CameraCache.POV.Rotation;
+
+
+	//	FVector end = RotationToVector(camRotation);
+
+	//	FVector camLoc = debugCameraHud->PlayerOwner->PlayerCamera->CameraCache.POV.location;
+	//	end.X = end.X * 5000 * 20 + camLoc.X;
+	//	end.Y = end.Z * 5000 * 20 + camLoc.Y;
+	//	end.Z = end.Z * 5000 * 20 + camLoc.Z;
+
+	//	AActor* foundActor = debugCameraHud->Trace(end, camLoc, 1, Extent, 0, &HitLocation, &HitNormal, &HitInfo);
+	//	if (foundActor)
+	//	{
+	//		string str = string_format("Containing file: %s.pcc", foundActor->GetContainingMapName());
+	//		std::wstring widestr = std::wstring(str.begin(), str.end());
+	//		debugCameraHud->Canvas->DrawText(FString(const_cast<wchar_t*>(widestr.c_str())), true, 1.0f, 1.0f, NULL);
+
+	//		ypos += 12;
+	//		debugCameraHud->Canvas->SetPos(xpos, ypos); //set canvas position to (10,10)
+	//		str = string_format("Hit Actor: %s_%i", foundActor->GetFullNameNoClass(), (foundActor->Name.GetIndex() - 1));
+	//		widestr = std::wstring(str.begin(), str.end());
+	//		debugCameraHud->Canvas->DrawText(FString(const_cast<wchar_t*>(widestr.c_str())), true, 1.0f, 1.0f, NULL);
+
+
+	//		if (HitInfo.HitComponent) {
+	//			ypos += 12;
+	//			debugCameraHud->Canvas->SetPos(xpos, ypos); //set canvas position to (10,10)
+	//			str = string_format("Hit component: %s_%d", HitInfo.HitComponent->GetFullName(), HitInfo.HitComponent->Name.GetIndex() - 1);
+	//			widestr = std::wstring(str.begin(), str.end());
+	//			debugCameraHud->Canvas->DrawText(FString(const_cast<wchar_t*>(widestr.c_str())), false, 1.0f, 1.0f, NULL);
+	//		//	HitInfo.HitComponent->
+	//		}
+
+	//		//logger.writeToLog(string_format("(%s) %s_%d\n", mapname, fullname, instanceIndex), true);
+	//	}
+	//	else
+	//	{
+	//		debugCameraHud->Canvas->DrawText(L"No trace hit", true, 1.0f, 1.0f, NULL);
+
+	//	}
+	//	return; // do not process twice.
+	//	/*AActor* actor = (AActor*)pObject;
+	//	if (actor->WorldInfo)
+	//	{
+	//		if (actor->WorldInfo->bUseConsoleInput == 0)
+	//		{
+	//			logger.writeToConsoleOnly(string_format(" >> WI IS ZERO! WorldInfo.bUseConsoleInput: %u\n", actor->WorldInfo->bUseConsoleInput), true);
+	//		}
+	//		actor->WorldInfo->bUseConsoleInput = 1;
+	//	}*/
 	//}
+	//else 
+	if (isPartOf(szName, "Function SFXGame.SFXConsole.InputKey")) {
+		USFXConsole_execInputKey_Parms* params = (USFXConsole_execInputKey_Parms*)pParms;
+		//Event: 0 - > Down
+		//Event: 1 - > Up
+		//Event: 2 - > Holding
+		//logger.writeToConsoleOnly(string_format("Key pressed: %u %s\n", params->Event, params->Key.GetName()), true);
+		if (isPartOf(params->Key.GetName(), "Xbox")) {
+			if (params->Event == 0) {
+				logger.writeToConsoleOnly(string_format("%s DOWN\n", params->Key.GetName()), true);
+			}
+			else if (params->Event == 1) {
+				logger.writeToConsoleOnly(string_format("%s UP\n", params->Key.GetName()), true);
+			}
+			else if (params->Event == 2) {
+				logger.writeToConsoleOnly(string_format("%s HOLD\n", params->Key.GetName()), true);
+			}
+		}
+	}
 	ProcessEvent(pObject, pFunction, pParms, pResult);
 }
 
