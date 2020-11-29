b564b1f0ec00c46f3bdf99d14a8151bbe72df1b0
Mgamerz <mgamerz2@gmail.com>
2020-11-29 15:46:21 -0700

    Update streaming levels HUD

--- SteamingLevelsHUD/StreamingLevelsHUD.cpp
+++ SteamingLevelsHUD/StreamingLevelsHUD.cpp
@@ -7,6 +7,9 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #pragma comment(lib, "detours.lib") //Library needed for Hooking part.
+#pragma comment( lib, "psapi.lib" )
+#include <psapi.h>
+
 
 //typedef void(__thiscall* tProcessEvent)(class UObject*, class UFunction*, void*, void*);
 //tProcessEvent ProcessEvent = (tProcessEvent)0x00453120;
@@ -34,6 +37,10 @@ ME3TweaksASILogger logger("Streaming Levels HUD", "StreamingLevelsHUD.txt");
 //	return ss;
 //}
 
+const size_t MEDIUM_MEM = 3006477107; //2.8 GiB
+const size_t HIGH_MEM = 3543348019; //3.3 GiB
+size_t MaxMemoryHit = 0;
+
 static void RenderTextSLH(wstring msg, const float x, const float y, const float r, const float g, const float b, UCanvas* can)
 {
 	can->SetDrawColor(BYTE(r) * 255, BYTE(g) * 255, BYTE(b) * 255, 255);
@@ -56,6 +63,20 @@ static void RenderTextSLH(wstring msg, const float x, const float y, const float
 	can->DrawText(FString(msg.data()), 1, 1.0f, 1.0f, &renderInfo);
 }
 
+const char* FormatBytes(size_t bytes, char* keepInStackStr)
+{
+	const char* sizes[4] = { "B", "KB", "MB", "GB" };
+
+	int i;
+	double dblByte = bytes;
+	for (i = 0; i < 4 && bytes >= 1024; i++, bytes /= 1024)
+		dblByte = bytes / 1024.0;
+
+	sprintf(keepInStackStr, "%.2f", dblByte);
+
+	return strcat(strcat(keepInStackStr, " "), sizes[i]);
+}
+
 void __fastcall HookedPE(UObject* pObject, void* edx, UFunction* pFunction, void* pParms, void* pResult)
 {
 	char* funcName = pFunction->GetFullName();
@@ -67,12 +88,68 @@ void __fastcall HookedPE(UObject* pObject, void* edx, UFunction* pFunction, void
 		//numTicks++;
 		//if (numTicks > ticksBetweenUpdates) {
 			// Get list of streaming levels
+		// Render mem usage
+		PROCESS_MEMORY_COUNTERS pmc;
+		if (GetProcessMemoryInfo(OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, GetCurrentProcessId()), &pmc, sizeof(pmc)))
+		{
+			float r = 0;
+			float g = 0;
+
+			if (pmc.PagefileUsage > MEDIUM_MEM && pmc.PagefileUsage < HIGH_MEM)
+			{
+				// Yellow
+				r = 1;
+				g = 1;
+			}
+			else if (pmc.PagefileUsage >= HIGH_MEM)
+			{
+				//Red
+				r = 1;
+				g = 0;
+			}
+			else
+			{
+				// Green
+				g = 1;
+			}
+			char str[18] = ""; //Allocate
+			RenderTextSLH(s2ws(string_format("Memory usage: %s (%u bytes)", FormatBytes(pmc.PagefileUsage, str), pmc.PagefileUsage)), 5, 0, r, g, 0, hud->Canvas);
+
+			// Max Hit
+			if (pmc.PagefileUsage > MaxMemoryHit)
+			{
+				MaxMemoryHit = pmc.PagefileUsage;
+			}
+
+			if (MaxMemoryHit > MEDIUM_MEM && MaxMemoryHit < HIGH_MEM)
+			{
+				// Yellow
+				r = 1;
+				g = 1;
+			}
+			else if (MaxMemoryHit >= HIGH_MEM)
+			{
+				//Red
+				r = 1;
+				g = 0;
+			}
+			else
+			{
+				// Green
+				g = 1;
+			}
+			
+			char str2[18] = "";
+			RenderTextSLH(s2ws(string_format("Max memory hit: %s (%u bytes)", FormatBytes(MaxMemoryHit, str2), MaxMemoryHit)), 5, 12, r, g, 0, hud->Canvas);
+			
+		}
+
 		if (hud->WorldInfo)
 		{
 			//screenLogger->ClearMessages();
 			//logger.writeToConsoleOnly(string_format("Number of streaming levels: %d\n", hud->WorldInfo->StreamingLevels.Count), true);
 			if (hud->WorldInfo->StreamingLevels.Any()) {
-				int yIndex = 0;
+				int yIndex = 3; //Start at line 3 (starting at 0)
 				for (int i = 0; i < hud->WorldInfo->StreamingLevels.Count; i++) {
 					wstringstream ss;
 					auto sl = hud->WorldInfo->StreamingLevels.Data[i];
