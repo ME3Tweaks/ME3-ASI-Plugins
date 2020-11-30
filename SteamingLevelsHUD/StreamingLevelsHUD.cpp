1536457638a9c15864dbe4fee26ff96009aa5ea7
Mgamerz <mgamerz2@gmail.com>
2020-11-29 22:45:06 -0700

    Improve StreamingLevelsHUD

--- SteamingLevelsHUD/StreamingLevelsHUD.cpp
+++ SteamingLevelsHUD/StreamingLevelsHUD.cpp
@@ -1,55 +1,37 @@
 #include <string>
 #include <sstream>
-//#include "../ME3SDK/SdkHeaders.h"
 #include "../ME3SDK/ME3TweaksHeader.h"
 #include "../detours/detours.h"
-#include "../ME3SDK/ScreenLogger.h"
-
 #define _CRT_SECURE_NO_WARNINGS
 #pragma comment(lib, "detours.lib") //Library needed for Hooking part.
 #pragma comment( lib, "psapi.lib" )
 #include <psapi.h>
 
-
-//typedef void(__thiscall* tProcessEvent)(class UObject*, class UFunction*, void*, void*);
-//tProcessEvent ProcessEvent = (tProcessEvent)0x00453120;
-
-ME3TweaksASILogger logger("Streaming Levels HUD", "StreamingLevelsHUD.txt");
-//ScreenLogger screenLogger(L"Streaming Levels HUD v1");
-//
-//int numTicks = 0;
-//int ticksBetweenUpdates = 60;
-//
-//wstringstream& operator<<(wstringstream& ss, const FString& fStr)
-//{
-//
-//	for (auto i = 0; i < fStr.Num() && fStr(i) != 0; i++)
-//	{
-//		ss << fStr(i);
-//	}
-//	return ss;
-//}
-//
-//wstringstream& operator<<(wstringstream& ss, const FVector& vec)
-//{
-//	ss << "(" << vec.X << ", " << vec.Y << ", " << vec.Z << ")";
-//
-//	return ss;
-//}
-
 const size_t MEDIUM_MEM = 3006477107; //2.8 GiB
 const size_t HIGH_MEM = 3543348019; //3.3 GiB
-size_t MaxMemoryHit = 0;
+const size_t CRIT_HIGH_MEM = 3650722201; //3.4 GiB - on verge of crash
 
-static void RenderTextSLH(wstring msg, const float x, const float y, const float r, const float g, const float b, UCanvas* can)
+// Testing vars
+//const size_t MEDIUM_MEM = 30064771; //2.8 GiB
+//const size_t HIGH_MEM = 35433480; //3.3 GiB
+//const size_t CRIT_HIGH_MEM = 36507222; //3.4 GiB - on verge of crash
+
+size_t MaxMemoryHit = 0;
+int currentCritTick = 0;
+int critNumTicksToFlip = 10; //start
+const int critNumTicksToTurnOff = 30;
+const int critNumTicksToTurnOn = 30;
+BOOL critVisible = true;
+
+static void RenderTextSLH(std::wstring msg, const float x, const float y, const char r, const char g, const char b, const float alpha, UCanvas* can)
 {
-	can->SetDrawColor(BYTE(r) * 255, BYTE(g) * 255, BYTE(b) * 255, 255);
+	can->SetDrawColor(r, g, b, alpha * 255);
 	can->SetPos(x, y);
 	FLinearColor drawColor;
 	drawColor.R = r;
 	drawColor.G = g;
 	drawColor.B = b;
-	drawColor.A = 1.0f;
+	drawColor.A = alpha;
 	FVector2D glowBorder;
 	glowBorder.X = 2;
 	glowBorder.Y = 2;
@@ -82,38 +64,49 @@ void __fastcall HookedPE(UObject* pObject, void* edx, UFunction* pFunction, void
 	char* funcName = pFunction->GetFullName();
 	if (strcmp(funcName, "Function SFXGame.BioHUD.PostRender") == 0)
 	{
+		currentCritTick++;
+		if (currentCritTick > critNumTicksToFlip)
+		{
+			currentCritTick = 0;
+			critVisible = !critVisible;
+			critNumTicksToFlip = critVisible ? critNumTicksToTurnOff : critNumTicksToTurnOn;
+		}
 		const auto hud = static_cast<ABioHUD*>(pObject);
-		//screenLogger.PostRenderer(hud);
-
-		//numTicks++;
-		//if (numTicks > ticksBetweenUpdates) {
-			// Get list of streaming levels
 		// Render mem usage
 		PROCESS_MEMORY_COUNTERS pmc;
 		if (GetProcessMemoryInfo(OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, GetCurrentProcessId()), &pmc, sizeof(pmc)))
 		{
-			float r = 0;
-			float g = 0;
+			unsigned char r = 0;
+			unsigned char g = 0;
 
 			if (pmc.PagefileUsage > MEDIUM_MEM && pmc.PagefileUsage < HIGH_MEM)
 			{
 				// Yellow
-				r = 1;
-				g = 1;
+				r = 255;
+				g = 255;
 			}
 			else if (pmc.PagefileUsage >= HIGH_MEM)
 			{
 				//Red
-				r = 1;
+				r = 255;
 				g = 0;
 			}
 			else
 			{
 				// Green
-				g = 1;
+				g = 255;
 			}
+
+			// Flash if high crit
+
 			char str[18] = ""; //Allocate
-			RenderTextSLH(s2ws(string_format("Memory usage: %s (%u bytes)", FormatBytes(pmc.PagefileUsage, str), pmc.PagefileUsage)), 5, 0, r, g, 0, hud->Canvas);
+			float alpha = 1.0f;
+			if (pmc.PagefileUsage >= CRIT_HIGH_MEM && !critVisible)
+			{
+				alpha = 0.5;
+			}
+			RenderTextSLH(s2ws(string_format("Memory usage: %s (%u bytes)", FormatBytes(pmc.PagefileUsage, str), pmc.PagefileUsage)), 5.0f, 0.0f, r, g, 0, alpha, hud->Canvas);
+
 
 			// Max Hit
 			if (pmc.PagefileUsage > MaxMemoryHit)
@@ -124,24 +117,24 @@ void __fastcall HookedPE(UObject* pObject, void* edx, UFunction* pFunction, void
 			if (MaxMemoryHit > MEDIUM_MEM && MaxMemoryHit < HIGH_MEM)
 			{
 				// Yellow
-				r = 1;
-				g = 1;
+				r = 255;
+				g = 255;
 			}
 			else if (MaxMemoryHit >= HIGH_MEM)
 			{
 				//Red
-				r = 1;
+				r = 255;
 				g = 0;
 			}
 			else
 			{
 				// Green
-				g = 1;
+				g = 255;
 			}
-			
+
 			char str2[18] = "";
-			RenderTextSLH(s2ws(string_format("Max memory hit: %s (%u bytes)", FormatBytes(MaxMemoryHit, str2), MaxMemoryHit)), 5, 12, r, g, 0, hud->Canvas);
-			
+			RenderTextSLH(s2ws(string_format("Max memory hit: %s (%u bytes)", FormatBytes(MaxMemoryHit, str2), MaxMemoryHit)), 5.0f, 12.0f, r, g, 0, 1.0f, hud->Canvas);
+
 		}
 
 		if (hud->WorldInfo)
@@ -152,11 +145,11 @@ void __fastcall HookedPE(UObject* pObject, void* edx, UFunction* pFunction, void
 				int yIndex = 3; //Start at line 3 (starting at 0)
 				for (int i = 0; i < hud->WorldInfo->StreamingLevels.Count; i++) {
 					wstringstream ss;
-					auto sl = hud->WorldInfo->StreamingLevels.Data[i];
+					ULevelStreaming* sl = hud->WorldInfo->StreamingLevels.Data[i];
 					if (sl->bShouldBeLoaded || sl->bIsVisible) {
-						float r = 1;
-						float g = 1;
-						float b = 1;
+						unsigned char r = 255;
+						unsigned char g = 255;
+						unsigned char b = 255;
 
 						ss << sl->PackageName.GetName();
 						if (sl->PackageName.GetIndex() > 0)
@@ -167,29 +160,36 @@ void __fastcall HookedPE(UObject* pObject, void* edx, UFunction* pFunction, void
 						{
 							ss << " Visible";
 							r = 0;
-							g = 1;
+							g = 255;
 							b = 0;
+						}
+						else if (sl->bHasLoadRequestPending)
+						{
+							ss << " Loading";
+							r = 255;
+							g = 229;
+							b = 0;
+						}
+						else if (sl->bHasUnloadRequestPending)
+						{
+							ss << " Unloading";
+							r = 0;
+							g = 255;
+							b = 229;
 						}
 						else if (sl->bShouldBeLoaded)
 						{
 							ss << " Loaded";
-							r = 1;
-							g = 1;
+							r = 255;
+							g = 255;
 							b = 0;
 						}
 						const wstring msg = ss.str();
-						RenderTextSLH(msg, 5, yIndex * 12, r, g, b, hud->Canvas);
+						RenderTextSLH(msg, 5, yIndex * 12.0f, r, g, b, 1.0f, hud->Canvas);
 						yIndex++;
-						//screenLogger.LogMessage(msg);
 					}
 				}
 			}
-		}
-		//numTicks -= ticksBetweenUpdates;
-
-		else
-		{
-			logger.writeToConsoleOnly(L"No world info!\n", true);
 		}
 	}
 
